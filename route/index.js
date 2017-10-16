87eeab884572908a245a70ad1191543659238eb2
zjy <519659896@qq.com>
2017-10-16 20:45:11 +0800

    1

--- route/index.js
+++
@@ -1,8 +0,0 @@
-var user = require('./user');
-var file = require('./file');
-var web = require('./web');
-module.exports = app => {
-	app.use('/user',user)
-	app.use('/file',file)
-	app.use('/web',web)
-}
